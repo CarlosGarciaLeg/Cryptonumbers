3f2388c96dfd655f28c1f5d6c4ecf14c2c5ad831
nicoarkano <nicoarkano@gmail.com>
2021-08-16 16:40:40 -0300

    initial deployment

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftmarketaddress = "0xDFC41A93fac5A56C0fA64726ea01258c45BFa346"
+export const nftaddress = "0x3C58D9F5b2a01314c9f521225C220114288214eE"
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,3 @@
-/* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
 const fs = require('fs')
 const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
